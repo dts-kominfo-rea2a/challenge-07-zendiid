afbf097de927c18d387b0d7f4b7efb6a9c27a871
Zendi Cahya Y <zendicy@gmail.com>
2022-06-13 13:35:45 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (ekspresi) => {
+  let paramFungsi = (
+    await promiseTheaterIXX()).concat((await promiseTheaterVGC())
+    );
+
+  return paramFungsi.filter(
+    data => (data.hasil === ekspresi)).length;
+};;
 
 module.exports = {
   promiseOutput,
